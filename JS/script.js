d99a80ddca2c3e1ee490f1adb393c87591304af3
init.defaultBranch <andrewgscott@gmail.com>
2024-02-08 17:24:47 +0100

    working aside from anchor tags which currently display as text

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,41 @@
-
-
 const langButton = document.querySelector('#lang-switch');
-const textToChange = document.querySelectorAll('[data-lang]');
-// const enText = document.querySelectorAll('[data-lang="en"]');
 
-const handleLangClick = () => {
-  textToChange.forEach(element => {
-    element.style.display = element.style.display === 'none' ? '' : 'none';
-    element.style.visibility = element.style.visibility === 'hidden' ? 'visible' : 'hidden';
+
+// Function to update content based on selected language
+const updateContent = (langData) => {
+  document.querySelectorAll('[data-i18n]').forEach(element => {
+      const key = element.getAttribute('data-i18n');
+      console.log(key);
+      element.innerText = langData[key];
   });
 }
 
+
+// Function to fetch language data
+const fetchLanguageData = async(lang) => {
+  const response = await fetch(`Languages/${lang}.json`);
+  return response.json();
+}
+
+// Function to change language
+async function changeLanguage(lang) {
+  
+  const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+}
+
+const handleLangClick = () => {
+console.log('in handleLangClick');
+console.log('lang = ' + lang);
+  lang = lang === 'en' ? 'fr' : 'en';
+console.log('lang = ' + lang);
+  changeLanguage(lang);
+
+  
+}
+
 langButton.addEventListener('click', handleLangClick);
+
+// Default to English
+let lang = 'en';
+changeLanguage(lang);
